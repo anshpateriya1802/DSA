983f9283017ab64d59f9320b8dca3dc23d6da468
smartcraze <smartcraze354@gmail.com>
2024-10-08 01:09:54 +0530

    Add code to convert an array into a singly linked list

---
+++ Suraj/Singly-linked-list/convert_array-ll.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Node {
+public:
+    public:
+    int data;
+    Node* next;
+    Node(int data) {
+        this->data = data;
+        this->next = NULL;
+    }
+    Node(int data , Node* next){
+        this->data = data;
+        this->next = next;
+    }
+};
+
+int main() {
+    vector<int> arr = {5, 2, 9, 1, 5, 6};
+    
+    
+    return 0;
+}
