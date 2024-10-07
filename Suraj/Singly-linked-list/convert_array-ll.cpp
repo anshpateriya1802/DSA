1b1e66fe215b94baed364fbe4b390ec48dc10a22
smartcraze <smartcraze354@gmail.com>
2024-10-08 01:30:29 +0530

    Refactor arrayToLinkedList function and add lengthOfLinkedList function

--- Suraj/Singly-linked-list/convert_array-ll.cpp
+++ Suraj/Singly-linked-list/convert_array-ll.cpp
@@ -31,7 +31,7 @@ Node*arrayToLinkedList(int arr[],int size){
     Node* head = new Node(arr[0]);
     Node* current = head ;
 
-    for (int i = 1; i < size; i++)
+    for (int i = 1; i <= size; i++)
     {
         current->next = new Node(arr[i]);
         current = current->next;
@@ -39,6 +39,16 @@ Node*arrayToLinkedList(int arr[],int size){
     return head;
 }
 
+int lengthOfLinkedList(Node* head){
+    Node* temp = head;
+    int size = 0;
+    
+    while (temp->next != nullptr){
+        size++;
+        temp = temp->next;
+    }
+    return size;
+}
 
 
 
@@ -50,6 +60,8 @@ int main() {
 
     // Print the linked list
     printLinkedList(head);
+    int length = lengthOfLinkedList(head);
+    cout << "Length of the linked list: " << length << endl;
     
     return 0;
 }
