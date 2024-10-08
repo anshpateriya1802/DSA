9073cfcc19600bd544ae4b1656832da4604c0d7e
anshpateriya1802 <pateriyaansh1802@gmail.com>
2024-10-08 18:41:08 +0530

    insertion sort in python by ansh

---
+++ python/sorting/insertionSort.py
@@ -0,0 +1,13 @@
+class Solution:
+    def insertionSort(self, nums):
+        n=len(nums)
+        for i in range(n):
+            for j in range(i,0,-1):
+                if(nums[j]<nums[j-1]):
+                    nums[j],nums[j-1]=nums[j-1],nums[j]
+        return nums
+if __name__=="__main__":
+    nums=[64, 25, 12, 22, 11]
+    s=Solution()
+    print(s.insertionSort(nums))
+    # by ANSH PATERIYA
