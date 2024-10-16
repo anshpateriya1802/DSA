f119399575aba3353cfea57bd9a8574ef87125e1
anshpateriya1802 <pateriyaansh1802@gmail.com>
2024-10-16 23:41:39 +0530

    left rotate by k places in python

---
+++ Ansh/Array/python/leftRotateByKPlaces.py
@@ -0,0 +1,36 @@
+class Solution:
+    def rotateArray(self, nums, k):
+        n = len(nums) 
+        k = k % n  
+
+        temp = []
+
+        for i in range(k):
+            temp.append(nums[i])
+
+        for i in range(k, n):
+            nums[i - k] = nums[i]
+
+        for i in range(k):
+            nums[n - k + i] = temp[i]
+
+
+def printArray(nums):
+    for val in nums:
+        print(val, end=" ")
+    print()
+
+
+if __name__ == "__main__":
+    nums = [1, 2, 3, 4, 5, 6]
+    k = 2
+
+    print("Initial array: ")
+    printArray(nums)
+
+    sol = Solution()
+
+    sol.rotateArray(nums, k)
+
+    print(f"Array after rotating elements by {k} places: ")
+    printArray(nums)
