24616e468d35ad63b351fdfcf580ad7a95f58aa2
anshpateriya1802 <pateriyaansh1802@gmail.com>
2025-04-07 10:16:27 +0530

    iterative method for postorder

--- Ansh/Binary tree/postOrder.py
+++ Ansh/Binary tree/postOrder.py
@@ -1,3 +1,5 @@
+# recursive method
+
 # Definition for a binary tree node.
 # class TreeNode(object):
 #     def __init__(self, val=0, left=None, right=None):
@@ -17,4 +19,43 @@ class Solution:
         arr=[]
         self.postOrder(root,arr);
         return arr;
-        #your code goes here
+        
+
+
+
+# iterative method
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.data = val
+#         self.left = left
+#         self.right = right
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.data = val
+#         self.left = left
+#         self.right = right
+
+
+#iterative method
+
+class Solution:
+       
+        
+    def postorder(self, root):
+        result=[]
+        st=[]
+        if root:
+            st.append(root);
+        while st:
+            node=st.pop()
+            result.append(node.data)
+            if node.left:
+                st.append(node.left)
+            if node.right:
+                st.append(node.right)
+
+        result.reverse()
+        return result
